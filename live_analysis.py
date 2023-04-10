cae6934e9b27cc1f9c85f5113f671403443a4853
Yusuf <af003yusuf@gmail.com>
2023-04-10 18:49:49 +0700

    .

--- live_analysis.py
+++ live_analysis.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.express as px
 import re
 import numpy as np
 import streamlit as st
+import plotly.subplots as make_subplots
 columns = ['Duration','Revenue (Rp)','Products',	
            'Different Products Sold','Orders Created','Orders Paid',
            'Unit Sales','Buyers','Average Price (Rp)','CO rate',
            'Viewers','Views','ACU','PCU','Avg. Viewing Duration',
            'Comments','Shares','Likes','New Followers','Product Impressions',
-           'Product Clicks','CTR']
+           'Product Clicks','CTR','conversion']
 
-df = pd.read_excel('Live Analysis20230408061732 (live analysis last 7 days).xlsx',
+df = pd.read_excel('data2.xlsx',
                    sheet_name='Sheet2')
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.checkbox("Disable selectbox widget", key="disabled")
@@ -31,8 +33,16 @@ with col2:
         disabled=st.session_state.disabled,
     )
 
+with col3:
+    option2 = st.selectbox(
+        "Select KPI 2",
+        (columns),
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+    )
+
 datetime = pd.to_datetime(df['Launched Time'])
-datetime = datetime.dt.hour
+#datetime = datetime.dt.hour
 df['Launched Time'] = datetime
 
 for i in range(len(df)):
@@ -47,7 +57,13 @@ for i in range(len(df)):
 df['Duration']= pd.to_numeric(df['Duration'])
 df['CO rate'] = pd.to_numeric(df['CO rate'])
 df['CTR'] = pd.to_numeric(df['CTR'])
+df['conversion'] = df['Unit Sales']/df['Viewers']
+df['conversion'] = df['conversion'].fillna(0)
 df.sort_values(by='Launched Time',inplace=True)
-fig = go.Figure([go.Scatter(x=df['Launched Time'], y=df[option])])
 
+x = 'Launched Time'
+
+fig = px.line(df,x=x ,y=option)
+fig2 = px.line(df,x=x, y=option2)
 st.plotly_chart(fig)
+st.plotly_chart(fig2)
