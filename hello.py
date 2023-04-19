f902c8b4ae5b64e426a4ed14d8b0359c818aa896
Yusuf <af003yusuf@gmail.com>
2023-04-19 21:13:58 +0700

    .

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd 
+import os
 
 st.set_page_config(
     page_title="Hello",
@@ -12,5 +14,18 @@ st.sidebar.success("Select a menu above.")
 st.markdown(
     """
     still under construction, updates are underway
-"""
-)
+    
+    """ )
+
+path2 = 'data/core_stats'
+def save_uploadedfile(uploadedfile,path):
+     with open(os.path.join(path,uploadedfile.name),"wb") as f:
+         f.write(uploadedfile.getbuffer())
+     return st.success("Saved File:{} to core_stats".format(uploadedfile.name))
+
+datafile = st.file_uploader("Upload core statistic",type=['xlsx'])
+if datafile is not None:
+   file_details = {"FileName":datafile.name,"FileType":datafile.type}
+   df  = pd.read_excel(datafile)
+   st.dataframe(df)
+   save_uploadedfile(datafile,path2)
